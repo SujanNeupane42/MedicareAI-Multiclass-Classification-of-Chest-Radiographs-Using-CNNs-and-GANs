c101c31866d78eec27e6e6a3909a436e43645614
Sujan Neupane <85225585+Sujan42@users.noreply.github.com>
2022-12-20 11:16:42 +0545

    Dec 20 GoogLeNet

--- Models/GoogLeNet.py
+++ Models/GoogLeNet.py
@@ -13,30 +13,30 @@ def accuracy(outputs, labels):
     return round((accuracy.item() * 100),3)
 
 
-class Training(nn.Module):
-    def training_step(self, batch):
-        images, labels = batch 
-        out = self(images)                  # Generate predictions
-        loss = F.cross_entropy(out, labels) # Calculate loss
-        return loss
+# class Training(nn.Module):
+#     def training_step(self, batch):
+#         images, labels = batch 
+#         out = self(images)                  # Generate predictions
+#         loss = F.cross_entropy(out, labels) # Calculate loss
+#         return loss
     
-    def validation_step(self, batch):
-        images, labels = batch 
-        out = self(images)                    # Generate predictions
-        loss = F.cross_entropy(out, labels)   # Calculate loss
-        acc = accuracy(out, labels)           # Calculate accuracy
-        return {'val_loss': loss.detach(), 'val_acc': acc}
+#     def validation_step(self, batch):
+#         images, labels = batch 
+#         out = self(images)                    # Generate predictions
+#         loss = F.cross_entropy(out, labels)   # Calculate loss
+#         acc = accuracy(out, labels)           # Calculate accuracy
+#         return {'val_loss': loss.detach(), 'val_acc': acc}
         
-    def validation_epoch_end(self, outputs):
-        batch_losses = [x['val_loss'] for x in outputs]
-        epoch_loss = torch.tensor(batch_losses).mean()   # Combine losses
-        batch_accs = [x['val_acc'] for x in outputs]
-        epoch_acc = torch.tensor(batch_accs).mean()      # Combine accuracies
-        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+#     def validation_epoch_end(self, outputs):
+#         batch_losses = [x['val_loss'] for x in outputs]
+#         epoch_loss = torch.tensor(batch_losses).mean()   # Combine losses
+#         batch_accs = [x['val_acc'] for x in outputs]
+#         epoch_acc = torch.tensor(batch_accs).mean()      # Combine accuracies
+#         return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
     
-    def epoch_end(self, epoch, result):
-        print("Epoch [{}], last_lr: {:.5f}, train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
-            epoch, result['lrs'][-1], result['train_loss'], result['val_loss'], result['val_acc']))     
+#     def epoch_end(self, epoch, result):
+#         print("Epoch [{}], last_lr: {:.5f}, train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+#             epoch, result['lrs'][-1], result['train_loss'], result['val_loss'], result['val_acc']))     
     
 
 
