085b3fc7683f191b4316a9b839d8ec371a2a6326
Sujan Neupane <85225585+Sujan42@users.noreply.github.com>
2023-02-27 09:14:50 +0545

    UPDATED

--- WebApp_MYSQL_Integration/Classifier/template/script.js
+++ WebApp_MYSQL_Integration/Classifier/template/script.js
@@ -1,5 +1,8 @@
 const loadFile = function (event) {
-  // console.log("Hello world");
+  console.log("Hello world");
   var image = document.getElementById("output");
+  var condition = document.getElementById("div_to_remove");
+  console.log(condition);
+  condition.innerHTML = "";
   image.src = URL.createObjectURL(event.target.files[0]);
 };
